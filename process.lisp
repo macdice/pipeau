20e43513d326ff2a79a6fc0983e163968f19bd57
Thomas Munro <macdice@penski.net>
2012-04-03 07:39:16 +0100

    Added some sketches of trap-exit and link support (pseudocode for now)

--- process.lisp
+++ process.lisp
@@ -1,10 +1,12 @@
 (in-package :pipeau)
 
 (defstruct process
-  "A record type for processes, which link a mailbox and a thread."
+  "A record type for processes."
   (:name)
   (:thread)
-  (:mailbox))
+  (:mailbox)
+  (:links)
+  (:trap-exits))
 
 (defparameter *self* (make-process :name "main" 
                                    :mailbox (make-mailbox))
@@ -16,8 +18,11 @@
          (process (make-process :mailbox mailbox :name name))
          (thread (bordeaux-threads:make-thread 
                   (lambda ()
-                    (setf *self* process)
-                    (funcall function))
+                    (let ((*self* process)) ;; thread-local rebind of special
+                      (unwind-protect
+                           (funcall function)
+                        (loop for link in (process-links (self))
+                             do (! link `(EXIT ,(self)))))))
                   :name name)))
     (setf (process-thread process) thread) ;; a bit circular!
     process))
@@ -27,16 +32,17 @@
   (bordeaux-threads:join-thread (process-thread process)))
 
 (defun self ()
-  "Return the current process's mailbox."
+  "Return the current process."
   *self*)
+
+(defun trap-exits (&optional (active t))
+  "Register to receive exit messages from linked processes explicitly,
+rather than getting an error."
+  (setf (process-trap-exits active)))
 
 (defun ? (&optional timeout default)
   "Receive a message from one's own mailbox."
   (mailbox-receive-if (process-mailbox *self*) (lambda (x) t) timeout default))
-
-;;(defun ?* ()
-;;  "Receive all messages currently waiting in one's mailbox without blocking."
-;;  (sb-concurrency:receive-pending-messages (process-mailbox *self*)))
 
 (defun ! (process message)
   "Send a message to a process."
